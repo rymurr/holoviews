3aeb759f8f45cabd54fb1607918f5c5c5245294a
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-10-02 02:53:30 +0100

    Switched to bokeh protocol for static embedding

--- holoviews/plotting/bokeh/tabular.py
+++ holoviews/plotting/bokeh/tabular.py
@@ -24,8 +24,6 @@ class TablePlot(BokehPlot, GenericElementPlot):
         The hook is passed the plot object and the displayed
         object, and other plotting handles can be accessed via plot.handles.""")
 
-    _update_handles = ['source', 'glyph']
-
     def __init__(self, element, plot=None, **params):
         super(TablePlot, self).__init__(element, **params)
         self.handles = {} if plot is None else self.handles['plot']
@@ -83,32 +81,6 @@ class TablePlot(BokehPlot, GenericElementPlot):
         self.drawn = True
 
         return table
-
-
-    @property
-    def current_handles(self):
-        """
-        Returns a list of the plot objects to update.
-        """
-        handles = []
-        if self.static and not self.dynamic:
-            return handles
-
-
-        element = self.current_frame
-        previous_id = self.handles.get('previous_id', None)
-        current_id = None if self.current_frame is None else element._plot_id
-        for handle in self._update_handles:
-            if (handle == 'source' and self.dynamic and current_id == previous_id):
-                continue
-            if handle in self.handles:
-                handles.append(self.handles[handle])
-
-        # Cache frame object id to skip updating if unchanged
-        if self.dynamic:
-            self.handles['previous_id'] = current_id
-
-        return handles
 
 
     def update_frame(self, key, ranges=None, plot=None):
